dee1866b34ca50860c7271938a6761a97b684344
HazeL <105066838+HazeLdevgggg@users.noreply.github.com>
2023-06-27 16:05:41 +0200

    Update login.php

--- login.php
+++ login.php
@@ -33,10 +33,11 @@
 <?
     if (isset($_POST['login_username'], $_POST['login_password'])) {
         try {
-            $serveur = "sportmarludev.mysql.db"; 
-            $login = "sportmarludev"; 
-            $pass = "DevMadein34"; 
-            $connection = new PDO("mysql:host=$serveur;dbname=sportmarludev",$login, $pass); 
+            $serveur = "//"; 
+            $login = "//"; 
+            $pass = "//"; 
+            $dbname = "//"; 
+            $connection = new PDO("mysql:host=$serveur;dbname=$dbname,$login, $pass); 
             $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $stmt = $connection->prepare("SELECT username, password FROM user");
             $stmt->execute();
